dc0673d2bb6a91b9d201c7a420fe72c5211b6ade
Yelyzaveta Dolhova <liza.dolhova@gforces.pl>
2023-05-15 23:18:05 +0200

    Jest config fix

--- jest.config.js
+++ jest.config.js
@@ -14,4 +14,16 @@ module.exports = {
   testEnvironmentOptions: {
     customExportConditions: ['node', 'node-addons'],
   },
+  collectCoverageFrom: [
+    'src/**/*.{js,vue}',
+    '!**/node_modules/**',
+    '!src/**/index.js',
+    '!src/main.js',
+    '!src/registerServiceWorker.js',
+    '!src/sw.js',
+    '!src/helpers/*',
+    '!src/router/**',
+    '!src/lang/**',
+    '!src/plugins/**',
+  ],
 };
